dd811a543a621cf15916308e84557aad78a0f176
root <root@ip-172-31-0-220.us-east-2.compute.internal>
2024-03-18 18:05:45 +0000

    Added Wordpress Website

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpressdb' );
+define( 'DB_NAME', 'wpdb' );
 
 /** Database username */
-define( 'DB_USER', 'wpuser1' );
+define( 'DB_USER', 'shrishti' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'wpPass@1' );
+define( 'DB_PASSWORD', 'Admin@123' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
